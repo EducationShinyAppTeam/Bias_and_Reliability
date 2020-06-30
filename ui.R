f40fec5975a98619fa09136d4d2268fb81af162d
Chenese Gray <64931237+cgray1117@users.noreply.github.com>
2020-06-30 01:07:26 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -3,8 +3,6 @@ library(shiny)
 library(shinydashboard)
 library(png)
 library(shinyBS)
-library(V8)
-library(shinyjs)
 library(shinyWidgets)
 library(boastUtils)
 
@@ -18,20 +16,19 @@ APP_DESCP  <<- paste(
 )
 # End App Meta Data------------------------------------------------------------
 
-#Use jscode to for reset button to reload the app
-jsResetCode <- "shinyjs.reset = function() {history.go(0)}"
-
-
 ui <- list(
   tags$head(
-    tags$link(rel = "stylesheet", type = "text/css",
-              href = "https://educationshinyappteam.github.io/Style_Guide/theme/boast.css")
+    tags$link(
+      rel = "stylesheet", 
+      type = "text/css",
+      href = "https://educationshinyappteam.github.io/Style_Guide/theme/boast.css")
   ),
     dashboardPage(
         skin = "red",
         dashboardHeader(
             title = "Bias & Reliability",
-            titleWidth = 220,
+            titleWidth = 250,
+            tags$li(class = "dropdown", actionLink("info", icon("info"))),
             tags$li(
                 class = "dropdown",
                 tags$a(
@@ -40,13 +37,12 @@ ui <- list(
                 )
             )
         ),
-        #adding prereq pages
         dashboardSidebar(
-            width = 220,
+            width = 250,
             sidebarMenu(id='tabs',
                 menuItem("Overview",tabName = "overview", icon = icon("dashboard")),
                 menuItem("Prerequisites", tabName= "prerequisites", icon=icon("book")),
-                menuItem("Game",tabName = "game", icon = icon("gamepad")),
+                menuItem("Challenge",tabName = "challenge", icon = icon("cog")),
                 menuItem("References", tabName = "references", icon = icon("leanpub"))
             ),
             tags$div(
@@ -59,40 +55,59 @@ ui <- list(
                 tabItem(
                     tabName = "overview",
                     h1("Bias & Reliability"),
-                    p("This app helps with understanding the concepts of bias and reliability."),
+                    p("This app helps with understanding the concepts of 
+                      bias and reliability."),
                     br(),
                     h2("Instructions"),
                     tags$ol(
-                        tags$li("Click the target to put down at least 10 points."),
-                        tags$li("Check the feedback and keep trying until you get it correct and go to the next question."),
-                        tags$li('Note: There are only four different questions.')),
+                        tags$li("Read the challenge and think about how you might achieve it."),
+                        tags$li("Click on the target to place points. You will need to put at least 10 points."),
+                        tags$li("Examine and use the provided feedback to place points until you meet the challenge."),
+                        tags$li("When you achieve the current challenge, press the Next button to try another. 
+                                (Note: there are only four unique challenges.")),
                     div(
                         style = "text-align: center",
                         bsButton(
                             inputId = "go",
                             label = "Go to Prerequisites",
                             icon = icon("bolt"),
-                            style = "danger",
                             size = "large",
-                            class="circle grow"
                         )
                     ),
                     br(),
                     br(),
                     h2("Acknowledgements"),
-                    p("This app was developed and coded by Yuxin Zhang and updated by Luxin Wang, Thomas McIntyre and Chenese Gray.")
+                    p("This app was developed and coded by Yuxin Zhang and 
+                      updated by Luxin Wang, Thomas McIntyre and Chenese Gray."),
+                    div(class = "updated", "Last Update: 6/25/2020 by CG.")
                 ),
-                        #Adding pre-requisites page to remove background from instructions page
+                #Adding pre-requisites page to remove background from instructions page
                 tabItem(
                     tabName = "prerequisites",
+                    h2("Prerequisites"),
+                    br(),br(),
                     h3("Review the following concepts: "),
                     br(),
                     tags$ul(
-                        tags$li("The bias of a measurement describes to what degree it is systematically off target from the true value.
-                        While playing with the app you should think of what gives you a high or low bias."),
-                        tags$li("The reliability of a measurement describes how consistent the measurement is when you repeat it 
-                        (alternatively, an unreliable measurement is one that shows a lot of variability from value to value 
-                        when the measurement is repeated independently). ")
+                        tags$li("Measurement is one of the main components of Statistics. 
+                                Whether we're measuring something about a single object 
+                                (e.g., the mass of a cookie), a single living being (e.g., 
+                                a person's height), or a collection through the use of a statistic 
+                                (e.g., how many people prefer Meyer Dairy over Berkey Creamery), 
+                                our measurements might vary."),
+                        
+                        tags$li(strong('Bias'), "refers to the degree by which a measurement target 
+                                systematically misses the true value of what you're measuring. 
+                                If the true value is the bull's eye of a target, how might 
+                                high and low values of bias impact where you hit the target?"),
+                        
+                        tags$li(strong('Reliability'), "refers to how consistent the measurement process 
+                                is. If you carry out the process in the exact same way on the 
+                                exact same object and then compare all of your measurements, 
+                                are they all the same (no variation), fairly close to each 
+                                each other (low variation), or really far from each other 
+                                (high variation)? Low variation indicates high reliability; 
+                                high variation indicates low reliability. ")
                     ),
                     div(
                         style = "text-align: center",
@@ -100,23 +115,14 @@ ui <- list(
                             inputId = "start",
                             label = "Go",
                             icon = icon("bolt"),
-                            style = "danger",
                             size = "large",
-                            class="circle grow"
                         )
                     )
                 ),        
                 tabItem(
-                    tabName ="game",
-                    div(
-                        style="display: inline-block;vertical-align:top;",
-                        circleButton(
-                            inputId = "info",
-                            icon = icon("info"), 
-                            status = "myClass",
-                            size = "xs"
-                        )
-                    ),
+                    tabName ="challenge",
+                    h2("Challenge Yourself!"),
+                    br(),
                     wellPanel(
                         fluidRow(uiOutput("question"))
                     ),
@@ -131,33 +137,30 @@ ui <- list(
                             style = "height: 320px;"
                         ),
                         column(
-                            width = 8, 
+                            width = 4, 
                             conditionalPanel(
                                 condition = 'input.submit != 0',
-                                fluidRow(
-                                    #change scroll over for bias and reliability
-                                    column(
-                                        width = 6,
-                                        plotOutput("plota"), 
-                                        style = "height: 320px;",
-                                        bsPopover(
-                                            id = "plota", 
-                                            title = "Bias", 
-                                            content = "How far the points from the center.",
-                                            placement = "top"
-                                        )
-                                    ),
-                                    column(
-                                        width = 6,
-                                        plotOutput("plotb"), 
-                                        style = "height: 320px;",
-                                        bsPopover(
-                                            id = "plotb", 
-                                            title = "Reliability", 
-                                            content = "How far points are from each other",
-                                            placement = "top"
-                                        )
-                                    )
+                                plotOutput("plota"), 
+                                style = "height: 320px;",
+                                bsPopover(
+                                    id = "plota", 
+                                    title = "Bias", 
+                                    content = "How far the points from the center.",
+                                    placement = "top"
+                                )
+                            )
+                        ),
+                        column(
+                            width = 4,
+                            conditionalPanel(
+                                condition = 'input.submit != 0',
+                                plotOutput("plotb"), 
+                                style = "height: 320px;",
+                                bsPopover(
+                                    id = "plotb", 
+                                    title = "Reliability", 
+                                    content = "How far points are from each other",
+                                    placement = "top"
                                 )
                             )
                         )
@@ -177,15 +180,13 @@ ui <- list(
                             ),
                             bsButton(
                                 inputId = "new",
-                                label = "Next>>", 
-                                style = "danger",
+                                label = "Next >>", 
                                 size = "large",
                                 disabled = TRUE
                             ),
                             bsButton(
                               inputId = 'clear', 
                               label = "Try Again", 
-                              style = 'danger', 
                               size = 'large'
                             )
                         ),
@@ -197,7 +198,6 @@ ui <- list(
                                 wellPanel(
                                     div(
                                         style = "position: relative; top:0",
-                                        print("Feedback")
                                     ),
                                     img(src = "arrow.gif", width = 40), 
                                     class = "arrow"
@@ -209,13 +209,14 @@ ui <- list(
                         hr(),
                         conditionalPanel(
                             "input.submit != 0",
-                            h3(uiOutput("answer")),
+                            div(
+                              style = "text-align: center",
+                              uiOutput("answer")),
                             wellPanel(
                                 uiOutput("feedback0"),
-                                uiOutput("feedback1"),
-                                uiOutput("feedback2"),
-                                uiOutput("feedback3"), 
-                                class = "wellfeedback"
+                                uiOutput("feedback1"),br(),
+                                uiOutput("feedback2"),br(),
+                                uiOutput("feedback3")
                             )
                         )
                     )
@@ -225,46 +226,67 @@ ui <- list(
                     h2("References"),
                     p(
                       class = "hangingindent",
-                      "Dean Attali (2020). shinyjs: Easily Improve the User Experience of Your 
-                      Shiny Apps in Seconds. R package version 1.1. https://CRAN.R-project.org/package=shinyjs"
+                      "Dean Attali (2020). shinyjs: Easily Improve the User 
+                      Experience of Your Shiny Apps in Seconds. R package 
+                      version 1.1. Available from https://CRAN.R-project.org/package=shinyjs"
                     ),
                     p(
                       class = "hangingindent",
-                      "Eric Bailey (2015). shinyBS: Twitter Bootstrap Components for Shiny. R package version 0.61.
+                      "Eric Bailey (2015). shinyBS: Twitter Bootstrap Components
+                      for Shiny. R package version 0.61. Available from
                       https://CRAN.R-project.org/package=shinyBS"
                     ),
                     p(
                       class = "hangingindent",
-                      "Jeroen Ooms (2020). V8: Embedded JavaScript and WebAssembly Engine for R. R package 
-                      version 3.0.2. https://CRAN.R-project.org/package=V8"
+                      "Jeroen Ooms (2020). V8: Embedded JavaScript and WebAssembly 
+                      Engine for R. R package version 3.0.2. Available from
+                      https://CRAN.R-project.org/package=V8"
                     ),
                     p(
                       class = "hangingindent",
-                      "Robert Carey and Neil Hatfield (2020). boastUtils: BOAST Utilities. R package version 0.1.4.
+                      "Robert Carey and Neil Hatfield (2020). boastUtils: BOAST Utilities. 
+                      R package version 0.1.4. Available from
                       https://github.com/EducationShinyAppTeam/boastUtils"
                     ),
                     p(
                       class = "hangingindent",
-                      "Simon Urbanek (2013). png: Read and write PNG images. R package version 0.1-7.
-                      https://CRAN.R-project.org/package=png"
+                      "Simon Urbanek (2013). png: Read and write PNG images. 
+                      R package version 0.1-7. Available from https://CRAN.R-project.org/package=png"
                     ),
                     p(
                       class = "hangingindent",
-                      "Victor Perrier, Fanny Meyer and David Granjon (2020). shinyWidgets: Custom Inputs Widgets for Shiny. R
-                      package version 0.5.2. https://CRAN.R-project.org/package=shinyWidgets"
+                      "Victor Perrier, Fanny Meyer and David Granjon (2020). shinyWidgets: 
+                      Custom Inputs Widgets for Shiny. R package version 0.5.2. Available from
+                      https://CRAN.R-project.org/package=shinyWidgets"
                     ),
                     p(
                       class = "hangingindent",
-                      "Winston Chang and Barbara Borges Ribeiro (2018). shinydashboard: Create Dashboards with 'Shiny'. R
-                      package version 0.7.1. https://CRAN.R-project.org/package=shinydashboard"
+                      "Winston Chang and Barbara Borges Ribeiro (2018). shinydashboard: 
+                      Create Dashboards with 'Shiny'. R package version 0.7.1. Available from
+                      https://CRAN.R-project.org/package=shinydashboard"
                     ),
                     p(
                       class = "hangingindent",
-                      "Winston Chang, Joe Cheng, JJ Allaire, Yihui Xie and Jonathan McPherson (2020). shiny: Web
-                      Application Framework for R. R package version 1.4.0.2. https://CRAN.R-project.org/package=shiny"
-                    )
+                      "Winston Chang, Joe Cheng, JJ Allaire, Yihui Xie and Jonathan 
+                      McPherson (2020). shiny: Web Application Framework for R. R 
+                      package version 1.4.0.2. Available from 
+                      https://CRAN.R-project.org/package=shiny"
+                    ),
+                    br(),
+                    br(),
+                    br(),
+                    boastUtils::copyrightInfo()
+                    
                 )
             )
         )
     )
 )
+
+
+
+
+
+
+
+
