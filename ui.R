6082c5408a2d5071183c08369c739e0aa823e6ad
Chenese Gray <64931237+cgray1117@users.noreply.github.com>
2020-09-03 14:19:03 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -60,11 +60,15 @@ ui <- list(
                     br(),
                     h2("Instructions"),
                     tags$ol(
-                        tags$li("Read the challenge and think about how you might achieve it."),
-                        tags$li("Click on the target to place points. You will need to put at least 10 points."),
-                        tags$li("Examine and use the provided feedback to place points until you meet the challenge."),
-                        tags$li("When you achieve the current challenge, press the Next button to try another. 
-                                (Note: there are only four unique challenges.")),
+                        tags$li("Read the challenge and think about how you might 
+                        achieve it."),
+                        tags$li("Click on the target to place points. You will 
+                        need to put at least 10 points."),
+                        tags$li("Examine and use the provided feedback to place 
+                        points until you meet the challenge."),
+                        tags$li("When you achieve the current challenge, press 
+                        the Next button to try another. (Note: there are only 
+                        four unique challenges.")),
                     div(
                         style = "text-align: center",
                         bsButton(
@@ -78,7 +82,8 @@ ui <- list(
                     br(),
                     h2("Acknowledgements"),
                     p("This app was developed and coded by Yuxin Zhang and 
-                      updated by Luxin Wang, Thomas McIntyre and Chenese Gray."),
+                      updated by Luxin Wang and Thomas McIntyre. The most recent
+                      version was updated by Chenese Gray."),
                     div(class = "updated", "Last Update: 6/25/2020 by CG.")
                 ),
                 #Adding pre-requisites page to remove background from instructions page
@@ -89,25 +94,28 @@ ui <- list(
                     h3("Review the following concepts: "),
                     br(),
                     tags$ul(
-                        tags$li("Measurement is one of the main components of Statistics. 
-                                Whether we're measuring something about a single object 
-                                (e.g., the mass of a cookie), a single living being (e.g., 
-                                a person's height), or a collection through the use of a statistic 
-                                (e.g., how many people prefer Meyer Dairy over Berkey Creamery), 
-                                our measurements might vary."),
+                        tags$li("Measurement is one of the main components of 
+                        Statistics. Whether we're measuring something about a 
+                        single object (e.g., the mass of a cookie), a single 
+                        living being (e.g., a person's height), or a collection 
+                        through the use of a statistic (e.g., how many people 
+                        prefer Meyer Dairy over Berkey Creamery),  measurements
+                        might vary."),
                         
-                        tags$li(strong('Bias'), "refers to the degree by which a measurement  
-                                systematically misses the true value of what you're measuring. 
-                                If the true value is the bull's eye of a target, how might 
-                                high and low values of bias impact where you hit the target?"),
+                        tags$li(strong('Bias'), "refers to the degree by which a
+                        measurement systematically misses the true value of what
+                        you're measuring. If the true value is the bull's eye of 
+                        a target, how might high and low values of bias impact 
+                        where you hit the target?"),
                         
-                        tags$li(strong('Reliability'), "refers to how consistent the measurement process 
-                                is. If you carry out the process in the exact same way on the 
-                                exact same object and then compare all of your measurements, 
-                                are they all the same (no variation), fairly close to each 
-                                each other (low variation), or really far from each other 
-                                (high variation)? Low variation indicates high reliability; 
-                                high variation indicates low reliability. ")
+                        tags$li(strong('Reliability'), "refers to how consistent 
+                        the measurement process is. If you carry out the process 
+                        in the exact same way on the exact same object and then 
+                        compare all of your measurements,are they all the same 
+                        (no variation), fairly close to each each other 
+                        (low variation), or really far from each other 
+                        (high variation)? Low variation indicates high reliability; 
+                        high variation indicates low reliability. ")
                     ),
                     div(
                         style = "text-align: center",
@@ -238,41 +246,40 @@ ui <- list(
                     ),
                     p(
                       class = "hangingindent",
-                      "Jeroen Ooms (2020). V8: Embedded JavaScript and WebAssembly 
+                      "Carey, R. and Hatfield, N. (2020). boastUtils: 
+                      BOAST Utilities. R package version 0.1.4. Available from
+                      https://github.com/EducationShinyAppTeam/boastUtils"
+                    ),
+                    p(
+                      class = "hangingindent",
+                      "Chang, W. and Borges Ribeiro, B. (2018). shinydashboard: 
+                      Create Dashboards with 'Shiny'. R package version 0.7.1. 
+                      Available from https://CRAN.R-project.org/package=shinydashboard"
+                    ),
+                    p(
+                      class = "hangingindent",
+                      "Chang, W., Cheng, J., Allaire, J., Xie, Y., and 
+                      McPherson, J. (2020). shiny: Web Application Framework for 
+                      R. R package version 1.4.0.2. Available from 
+                      https://CRAN.R-project.org/package=shiny"
+                    ),
+                    p(
+                      class = "hangingindent",
+                      "Ooms, J. (2020). V8: Embedded JavaScript and WebAssembly 
                       Engine for R. R package version 3.0.2. Available from
                       https://CRAN.R-project.org/package=V8"
                     ),
                     p(
                       class = "hangingindent",
-                      "Carey, R. and Hatfield, N. (2020). boastUtils: BOAST Utilities. 
-                      R package version 0.1.4. Available from
-                      https://github.com/EducationShinyAppTeam/boastUtils"
+                      "Perrier, V., Meyer, F., and Granjon, D. (2020). shinyWidgets: 
+                      Custom Inputs Widgets for Shiny. R package version 0.5.2. 
+                      Available from https://CRAN.R-project.org/package=shinyWidgets"
                     ),
                     p(
                       class = "hangingindent",
                       "Urbanek, S. (2013). png: Read and write PNG images. 
                       R package version 0.1-7. Available from https://CRAN.R-project.org/package=png"
-                    ),
-                    p(
-                      class = "hangingindent",
-                      "Perrier, V., Meyer, F., and Granjon, D. (2020). shinyWidgets: 
-                      Custom Inputs Widgets for Shiny. R package version 0.5.2. Available from
-                      https://CRAN.R-project.org/package=shinyWidgets"
-                    ),
-                    p(
-                      class = "hangingindent",
-                      "Chang, W. and Borges Ribeiro, B. (2018). shinydashboard: 
-                      Create Dashboards with 'Shiny'. R package version 0.7.1. Available from
-                      https://CRAN.R-project.org/package=shinydashboard"
-                    ),
-                    p(
-                      class = "hangingindent",
-                      "Chang, W., Cheng, J., Allaire, J., Xie, Y., and 
-                      McPherson, J. (2020). shiny: Web Application Framework for R. R 
-                      package version 1.4.0.2. Available from 
-                      https://CRAN.R-project.org/package=shiny"
                     )
-                    
                 )
             )
         )
