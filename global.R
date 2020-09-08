be4428e6e1b697b6b6bffe43211df8d03b36fbd4
neilhatfield <phiguy1114@gmail.com>
2020-09-08 12:03:42 -0400

    My Updates
    These are my updates based on Chenese's work.

--- global.R
+++
@@ -1,6 +0,0 @@
-index = sample(1:4,1)
-
-question = c('Try to create a model for large bias and low reliability.',
-             'Try to create a model for large bias and high reliability.',
-             'Try to create a model for no bias and low reliability.',
-             'Try to create a model for no bias and high reliability.')
